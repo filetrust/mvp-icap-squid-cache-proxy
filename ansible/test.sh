d3dae95a673345ccdf67ee3eb5a9c794ea051483
NourEddine <noureddinex@protonmail.com>
2020-08-25 14:59:51 +0200

    test

--- ansible/test.sh
+++ ansible/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd $(dirname $0)
 pip3 install --user ansible[azure]
+update-alternatives --install python /usr/bin/python3
 grep -e "$HOME/.local/bin" -e '$HOME/.local/bin' <(echo $PATH) || eval $(echo 'export PATH=$PATH:$HOME/.local/bin' | tee -a ~/.bashrc)
 echo -e "$HOME/.ssh/squidkey\n\n" | ssh-keygen
-ansible-playbook -i "$HOME/.ssh/squidkey" azure-squid.yaml -e testing=True
+ansible-playbook --private-key "$HOME/.ssh/squidkey" azure-squid.yaml -e testing=True
