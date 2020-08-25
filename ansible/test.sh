0f292b844270474164576bb7dd83be348bfc3f06
NourEddine <noureddinex@protonmail.com>
2020-08-25 16:10:43 +0200

    test

--- ansible/test.sh
+++ ansible/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $(dirname $0)
-pip3 install --user ansible[azure]
-update-alternatives --install python /usr/bin/python3
+pip3 install --user ansible[azure] packaging
+update-alternatives --install python python /usr/bin/python3 0
 grep -e "$HOME/.local/bin" -e '$HOME/.local/bin' <(echo $PATH) || eval $(echo 'export PATH=$PATH:$HOME/.local/bin' | tee -a ~/.bashrc)
 echo -e "$HOME/.ssh/squidkey\n\n" | ssh-keygen
 echo 'inventory = inventory.txt' >> ansible.cfg
