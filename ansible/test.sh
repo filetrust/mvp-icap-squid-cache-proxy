fd446906a61f69b699b478e6e45a6ca335736df0
NourEddine <noureddinex@protonmail.com>
2020-08-25 16:42:21 +0200

    test

--- ansible/test.sh
+++ ansible/test.sh
@@ -4,6 +4,6 @@ pip3 install ansible[azure] packaging
 update-alternatives --install python python /usr/bin/python3 0
 grep -e "$HOME/.local/bin" -e '$HOME/.local/bin' <(echo $PATH) || eval $(echo 'export PATH=$PATH:$HOME/.local/bin' | tee -a ~/.bashrc)
 echo -e "$HOME/.ssh/squidkey\n\n" | ssh-keygen
-echo 'inventory = inventory.txt' >> ansible.cfg
-echo 'localhost ansible_host=localhost ansible_connection=local ansible_python_interpreter=/usr/bin/python3' >> inventory.txt
+#echo 'inventory = inventory.txt' >> ansible.cfg
+#echo 'localhost ansible_host=localhost ansible_connection=local ansible_python_interpreter=/usr/bin/python3' >> inventory.txt
 ansible-playbook --private-key "$HOME/.ssh/squidkey" azure-squid.yaml -e testing=True -e vm_name=Squid-GH-Action-$RANDOM
