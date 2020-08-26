b302c0b0eeeda38ab4bc5ce41719565658cb3a5f
NourEddine <noureddinex@protonmail.com>
2020-08-26 10:28:29 +0200

    ssh key test

--- ansible/test.sh
+++ ansible/test.sh
@@ -3,7 +3,7 @@ cd $(dirname $0)
 pip3 install ansible[azure] packaging
 update-alternatives --install /usr/bin/python python /usr/bin/python3 0
 grep -e "$HOME/.local/bin" -e '$HOME/.local/bin' <(echo $PATH) || eval $(echo 'export PATH=$PATH:$HOME/.local/bin' | tee -a ~/.bashrc)
-echo -e "\n\n\n" | ssh-keygen
+echo -e "$(pwd)/id_rsa\n\n" | ssh-keygen
 #echo 'inventory = inventory.txt' >> ansible.cfg
 #echo 'localhost ansible_host=localhost ansible_connection=local ansible_python_interpreter=/usr/bin/python3' >> inventory.txt
-ansible-playbook azure-squid.yaml -e testing=True -e vm_name=Squid-GH-Action-$RANDOM
+ansible-playbook --private-key "$(pwd)/id_rsa" -e testing=True -e vm_name=Squid-GH-Action-$RANDOM azure-squid.yaml 
