24d4795f584c40b27e635ecbfadeded5aed1a75d
NourEddine <noureddinex@protonmail.com>
2020-08-27 12:07:15 +0200

    test error handling

--- ansible/test.sh
+++ ansible/test.sh
@@ -6,4 +6,5 @@ grep -e "$HOME/.local/bin" -e '$HOME/.local/bin' <(echo $PATH) || eval $(echo 'e
 echo -e "$(pwd)/id_rsa\n\n" | ssh-keygen
 #echo 'inventory = inventory.txt' >> ansible.cfg
 #echo 'localhost ansible_host=localhost ansible_connection=local ansible_python_interpreter=/usr/bin/python3' >> inventory.txt
-ansible-playbook --private-key "$(pwd)/id_rsa" -e testing=True -e vm_name=Squid-GH-Action-$RANDOM azure-squid.yaml 
+export VMNAME=Squid-GH-Action-$RANDOM
+ansible-playbook --private-key "$(pwd)/id_rsa" -e testing=True -e vm_name=$VMNAME azure-squid.yaml 
