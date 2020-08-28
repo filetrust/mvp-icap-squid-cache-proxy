53043ce7f19bc414f0bd5b6159a9ab4ca964035f
NourEddine <noureddinex@protonmail.com>
2020-08-28 16:13:53 +0200

    remove update-alternatives

--- ansible/test.sh
+++ ansible/test.sh
@@ -2,7 +2,6 @@
 set -e
 cd $(dirname $0)
 pip3 install ansible[azure] packaging
-update-alternatives --install /usr/bin/python python /usr/bin/python3 0
 grep -e "$HOME/.local/bin" -e '$HOME/.local/bin' <(echo $PATH) || eval $(echo 'export PATH=$PATH:$HOME/.local/bin' | tee -a ~/.bashrc)
 echo -e "$(pwd)/id_rsa\n\n" | ssh-keygen
 #echo 'inventory = inventory.txt' >> ansible.cfg
