80b99b9937183a119b921f03fbc7bea46123d9ad
NourEddine <noureddinex@protonmail.com>
2020-08-28 16:04:35 +0200

    use commit instead of random number + set -e

--- ansible/test.sh
+++ ansible/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 cd $(dirname $0)
 pip3 install ansible[azure] packaging
 update-alternatives --install /usr/bin/python python /usr/bin/python3 0
@@ -6,6 +7,6 @@ grep -e "$HOME/.local/bin" -e '$HOME/.local/bin' <(echo $PATH) || eval $(echo 'e
 echo -e "$(pwd)/id_rsa\n\n" | ssh-keygen
 #echo 'inventory = inventory.txt' >> ansible.cfg
 #echo 'localhost ansible_host=localhost ansible_connection=local ansible_python_interpreter=/usr/bin/python3' >> inventory.txt
-export VMNAME=Squid-GH-Action-$RANDOM
+export VMNAME=Squid-GH-Actions-$GITHUB_SHA
 echo  "$VMNAME" > vmname.txt
 ansible-playbook --private-key "$(pwd)/id_rsa" -e testing=True -e vm_name=$VMNAME azure-squid.yaml 
